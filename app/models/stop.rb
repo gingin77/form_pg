564680b26a2e821eb60b8f96285ace560bed7293
gingin77 <ginniehench@gmail.com>
2017-10-08 22:57:06 -0400

    I tested out a few things to check for redundancies in the model

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,19 +1,10 @@
 class Stop < ApplicationRecord
   require 'csv'
 
-  def self.as_csv
-    CSV.generate do |csv|
-      csv << column_names
-      all.each do |item|
-        csv << item.attributes.values_at(*column_names)
-      end
-    end
-  end
-
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
 
-      stop_hash = row.to_hash # exclude the price field
+      stop_hash = row.to_hash
       stop = Stop.where(id: stop_hash["id"])
 
       if stop.count == 1
@@ -23,15 +14,14 @@ class Stop < ApplicationRecord
       end # end if !stop.nil?
     end # end CSV.foreach
   end # end self.import(file)
-end # end class
 
+  # def self.as_csv
+  #   CSV.generate do |csv|
+  #     csv << column_names
+  #     all.each do |item|
+  #       csv << item.attributes.values_at(*column_names)
+  #     end
+  #   end
+  # end
 
-# require 'csv'
-
-# Format:
-# 2 Main Street: A series of architecturally and historically significant properties,many with lofts/residences along the second story. (Main St inside Downtown Loop)
-# number - up to space
-# name - up to colon
-# descr - up to parentheses
-# address - inside parentheses
-#
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171006133702) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stops", force: :cascade do |t|
@@ -21,4 +23,5 @@ ActiveRecord::Schema.define(version: 20171006133702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
